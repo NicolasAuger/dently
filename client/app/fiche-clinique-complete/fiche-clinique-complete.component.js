1c5665fcd619eebeef07909ef3261908900e2d76
Oscar Grainger <oscarvgrainger@gmail.com>
2017-01-18 21:29:01 +0100

    added up to 6
    ng-model en cours

--- client/app/fiche-clinique-complete/fiche-clinique-complete.component.js
+++ client/app/fiche-clinique-complete/fiche-clinique-complete.component.js
@@ -8,7 +8,32 @@ import routes from './fiche-clinique-complete.routes';
 export class FicheCliniqueCompleteComponent {
   /*@ngInject*/
   constructor() {
-    this.message = 'Hello';
+    this.partie1 = {
+      titre : "1. Motif de la consultation :",
+      Etat : "renseigne",
+      question2 : false
+    }
+    this.partie2 = {
+      titre : "2. Evaluation de la douleur :",
+      Etat : "renseigne",
+    }
+    this.partie3 = {
+      titre : "3. Environnement :",
+      Etat : "renseigne",
+    }
+    this.partie4 = {
+      titre : "4. Trouble de la posture :",
+      Etat : "renseigne",
+    }
+    this.partie5 = {
+      titre : "5. Trouble du sommeil :",
+      Etat : "renseigne",
+      question1 : "non",
+    }
+    this.partie6 = {
+      titre : "6. Troubles occulaires :",
+      Etat : "renseigne",
+    }
   }
 }
 
