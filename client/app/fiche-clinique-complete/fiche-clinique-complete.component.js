32fc8ed48398a7e3fd4433220466a170d7c3794f
Oscar Grainger <oscarvgrainger@gmail.com>
2017-01-19 12:32:40 +0100

    Corrected up to 5 included

--- client/app/fiche-clinique-complete/fiche-clinique-complete.component.js
+++ client/app/fiche-clinique-complete/fiche-clinique-complete.component.js
@@ -8,31 +8,41 @@ import routes from './fiche-clinique-complete.routes';
 export class FicheCliniqueCompleteComponent {
   /*@ngInject*/
   constructor() {
-    this.partie1 = {
-      titre : "1. Motif de la consultation :",
-      Etat : "renseigne",
-      question2 : false
-    }
-    this.partie2 = {
-      titre : "2. Evaluation de la douleur :",
-      Etat : "renseigne",
-    }
-    this.partie3 = {
-      titre : "3. Environnement :",
-      Etat : "renseigne",
-    }
-    this.partie4 = {
-      titre : "4. Trouble de la posture :",
-      Etat : "renseigne",
-    }
-    this.partie5 = {
-      titre : "5. Trouble du sommeil :",
-      Etat : "renseigne",
-      question1 : "non",
-    }
-    this.partie6 = {
-      titre : "6. Troubles occulaires :",
-      Etat : "renseigne",
+    this.douleurs = [
+      {name: "M. Temporal droit", value : "mTemporalDroit"},
+      {name: "M. Temporal gauche", value : "mTemporalGauche"},
+      {name: "M. Masséter droit", value:"mMasseterDroit"},
+      {name: "M. Masséter gauche", value : "mMasseterGauche"},
+      {name: "ATM droite", value : "atmDroite"},
+      {name: "ATM gauche", value : "atmGauche"},
+      {name: "Autre", value : "autre"}
+    ]
+    this.data = {
+      partie1 : {
+        titre : "1. Motif de la consultation :",
+        Etat : "renseigne",
+      },
+      partie2: {
+        titre : "2. Evaluation de la douleur :",
+        Etat : "renseigne",
+      },
+      partie3: {
+        titre : "3. Environnement :",
+        Etat : "renseigne",
+      },
+      partie4: {
+        titre : "4. Trouble de la posture :",
+        Etat : "renseigne",
+      },
+      partie5: {
+        titre : "5. Trouble du sommeil :",
+        Etat : "renseigne",
+        question1 : "non",
+      },
+      partie6: {
+        titre : "6. Troubles occulaires :",
+        Etat : "renseigne",
+      },
     }
     this.partie7 = {
       titre : "7. Habitudes nocives :",
@@ -53,15 +63,6 @@ export class FicheCliniqueCompleteComponent {
     this.partie11 = {
       titre : "11. Palpation(s) douloureuse(s) des muscles masticateurs, des ATM, et des muscles cervico-scapulaires :",
       Etat : "renseigne",
-      douleurs : [
-      {name: "M. Temporal droit", value : "mTemporalDroit"},
-      {name: "M. Temporal gauche", value : "mTemporalGauche"},
-      {name: "M. Masséter droit", value:"mMasseterDroit"},
-      {name: "M. Masséter gauche", value : "mMasseterGauche"},
-      {name: "ATM droite", value : "atmDroite"},
-      {name: "ATM gauche", value : "atmGauche"},
-      {name: "Autre", value : "autre"}
-      ]
     }
     this.partie12 = {
       titre : "12. Rapports incisifs :",
@@ -87,6 +88,6 @@ export default angular.module('dentlyApp.fiche-clinique-complete', [uiRouter])
   .component('ficheCliniqueComplete', {
     template: require('./fiche-clinique-complete.pug'),
     controller: FicheCliniqueCompleteComponent,
-    controllerAs: 'ficheCliniqueCompleteCtrl'
+    controllerAs: 'vm'
   })
   .name;
